3c9508381c392eab7027a28b719d728373ba5392
bboyKamel <nacoon@tlen.pl>
2019-02-18 13:37:18 +0100

    imlement getKeyFromValue

--- src/main/java/PlainTextMorseDictionary.java
+++ src/main/java/PlainTextMorseDictionary.java
@@ -1,4 +1,49 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PlainTextMorseDictionary {
+
+    private Map<String, String> dictionary;
+
+    public PlainTextMorseDictionary(){
+        dictionary = new HashMap<String, String>();
+        dictionary.put("A", ".-");
+        dictionary.put("B", "-...");
+        dictionary.put("C", "-.-.");
+        dictionary.put("D", "-..");
+        dictionary.put("E", ".");
+        dictionary.put("F", "..-.");
+        dictionary.put("G", "--.");
+        dictionary.put("H", "....");
+        dictionary.put("I", "..");
+        dictionary.put("J", ".---");
+        dictionary.put("K", "-.-");
+        dictionary.put("L", ".-..");
+        dictionary.put("M", "--");
+        dictionary.put("N", "-.");
+        dictionary.put("O", "---");
+        dictionary.put("P", ".--.");
+        dictionary.put("Q", "--.-");
+        dictionary.put("R", ".-.");
+        dictionary.put("S", "...");
+        dictionary.put("T", "-");
+        dictionary.put("U", "..-");
+        dictionary.put("V", "...-");
+        dictionary.put("W", ".--");
+        dictionary.put("X", "-..-");
+        dictionary.put("Y", "-.--");
+        dictionary.put("Z", "--..");
+    }
+
+
+    public String getKeyFromValue(String value) {
+        String key = null;
+        for (Map.Entry<String, String> entry : dictionary.entrySet()){
+            if (value.equals(entry.getValue())){
+                key = entry.getKey();
+            }
+        }
+
+        return key;
+    }
 }
