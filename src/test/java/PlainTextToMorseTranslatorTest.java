d16061eca52d3252c40c9c680a25dbb02d8430b3
bboyKamel <nacoon@tlen.pl>
2019-02-18 13:46:41 +0100

    create translateTextToMorse TEST

--- src/test/java/PlainTextToMorseTranslatorTest.java
+++ src/test/java/PlainTextToMorseTranslatorTest.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class PlainTextToMorseTranslatorTest {
+
+    @Test
+    public void shouldReturnSfromDicionaryTest() {
+        //given
+        final String signsToTranslate = "...";
+        final String expectedLetter = "S";
+        PlainTextMorseDictionary dictionary = new PlainTextMorseDictionary();
+
+        //when
+        String result = dictionary.getKeyFromValue(signsToTranslate);
+
+        //then
+        Assert.assertEquals(expectedLetter, result);
+    }
+
+    @Test
+    public void shoulTranslatePlainTextToMorse(){
+        //given
+        final String textToTranslate = "Litwo ojczyzno ty moja";
+        final String expextedResult = ".-.. .. - .-- --- / --- .--- -.-. --.. -.-- --.. -. --- / - -.-- / -- --- .--- .-";
+        MorseTranslator translator = new PlainTextToMorseTranslator();
+
+        //when
+        String result = translator.translate(textToTranslate);
+
+        //then
+        Assert.assertEquals(expextedResult, result);
+    }
+
+
 }
